b521cb20059a0e7c2bbc22307f777874c42f8242
G59-Toneli <gustavo.tonelI@outlook.com>
2025-09-27 17:38:03 -0300

    fix: Melhorias de UI

--- api3/components/ai_results/AiPage.tsx
+++ api3/components/ai_results/AiPage.tsx
@@ -39,14 +39,29 @@ export default function AIanswer() {
   }
 
   return (
-    <div>
-      <h1 className="font-bold">Resultado</h1>
-      <section className="pt-10">
-        <h3 className="font-semibold">
-          * Texto contendo o diagnostico basico fornecido pela Ia apos analisar
-          o form *
-        </h3>
-      </section>
+    <div className="flex flex-row w-full overflow-y-auto">
+      <div className="flex flex-col items-center w-full">
+        <h1 className="font-bold">Resultado</h1>
+        <section className="pt-10">
+          <h1 className="font-bold">👋 Olá !</h1>
+          <h2 className="font-semibold pt-6">
+            Agradecemos por compartilhar informações sobre a sua empresa! <br /><br />
+            O conteúdo que você verá a seguir foi construído a partir das suas
+            respostas e apresenta um retrato inicial do seu momento atual. Ele
+            evidencia tanto os pontos fortes que já sustentam sua trajetória
+            quanto as oportunidades de crescimento que podem gerar impacto
+            direto no engajamento, na produtividade e na capacidade de inovação
+            da sua equipe. <br />
+            Este diagnóstico não representa um fim, mas sim um
+            ponto de partida. Ele convida à reflexão sobre as práticas em
+            andamento e abre espaço para pensar em novos caminhos, capazes de
+            fortalecer ainda mais o desenvolvimento da sua organização.
+          </h2>
+          <h1 className="font-bold pt-20">
+            ☀️ Seu retrato inicial:
+          </h1>
+        </section>
+      </div>
     </div>
   );
 }
